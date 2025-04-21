4c28e7cea396d89479b4db8c3f1ca70fa47d5316
Salihu Abdulhamid <salihu45@gmail.com>
2025-04-21 05:44:15 +0100

    fix: 🐛 build fix

--- src/components/layout/hero.tsx
+++ src/components/layout/hero.tsx
@@ -52,13 +52,13 @@ export const Hero = () => {
 	const [isOpen, setIsOpen] = useState(false);
 	const [drawerOpen, drawerSetOpen] = useState(false);
 
-	//   useEffect(() => {
-	//     const interval = setInterval(() => {
-	//       drawerSetOpen(true);
-	//     }, 35000);
+	  useEffect(() => {
+	    const interval = setInterval(() => {
+	      drawerSetOpen(true);
+	    }, 35000);
 
-	//     return () => clearInterval(interval);
-	//   }, []);
+	    return () => clearInterval(interval);
+	  }, []);
 
 	const navItems = [
 		{
